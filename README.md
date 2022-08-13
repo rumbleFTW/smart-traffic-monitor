dc88dbc0cdce8f3a94af262dda9c7fe4fd07305c
Rajdeep Ghosh <85807431+rumbleFTW@users.noreply.github.com>
2022-08-13 21:53:28 +0530

    Integration

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+### To monitor the data run:
 
+```
+cd yolo && python traffic-monitor.py --source ../data/images/train/IMG20220811133939.jpg --weights 'runs/train/exp/weights/best.pt'  --save-crop
+```
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-import torch
-import numpy as np
+from easyocr import Reader
 import cv2
-import matplotlib.pyplot as plt
 
-import uuid
-import os
-import time
 
-##########################################################################
 
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='./yolo/runs/train/exp/weights/best.pt', force_reload=True)
+database = {'WB08R2567': {'Name': 'Rajdeep Ghosh', 'Address': 'Kolkata', 'Phone': 8240336721},
+            'JK04B8946': {'Name': 'Akash Kotal', 'Address': 'Midnapore', 'Phone': 9382795506}}
 
-img = './data/images/train/IMG20220811133825.jpg'
 
-pred = model(img)
 
-pred.print()
+img = cv2.imread('./yolo/runs/detect/exp3/crops/Numberplate/test1.jpg')
+
+reader = Reader(['de'])
+number = reader.readtext(img)
+number = 'JK04B8946'
+
+
+phoneNum = database[number]['Phone']
+
+
+print(f'Message sent to +91 {phoneNum}')
+
+
+
+
